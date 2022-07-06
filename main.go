310aad0462b7424d502614ebbf58c066be011e6d
manimovassagh <mani.mgh@gmail.com>
2022-07-06 20:25:57 +0200

    define types

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Block struct {
+}
+
+type Book struct {
+}
+
+type BookCheckout struct {
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", getBlockchain).Methods("GET")
 	r.Handle("/", writeBlock).Methods("POST")
 	r.Handle("/new", newBook).Methods("POST")
-
+	log.Println("Listening on Port 3000")
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
