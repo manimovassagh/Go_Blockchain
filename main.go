bd8c41fa98f387ba88d2fc9911ea5b6c87877aa8
manimovassagh <mani.mgh@gmail.com>
2022-07-06 21:17:36 +0200

    bookchckout

--- main.go
+++ main.go
@@ -18,6 +18,10 @@ type Block struct {
 }
 
 type BookCheckout struct {
+	BookID       string `json:"book_id"`
+	User         string `json:"user"`
+	CheckoutDate string `json:"checkout_date"`
+	IsGenesis    bool   `json:"is_genesis"`
 }
 
 type Blockchain struct {
@@ -28,9 +32,9 @@ var BlockChain *Blockchain
 
 func main() {
 	r := mux.NewRouter()
-	// r.HandleFunc("/", getBlockchain).Methods("GET")
-	// r.Handle("/", writeBlock).Methods("POST")
-	// r.Handle("/new", newBook).Methods("POST")
+	r.HandleFunc("/", getBlockchain).Methods("GET")
+	r.Handle("/", writeBlock).Methods("POST")
+	r.Handle("/new", newBook).Methods("POST")
 	log.Println("Listening on Port 3000")
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
